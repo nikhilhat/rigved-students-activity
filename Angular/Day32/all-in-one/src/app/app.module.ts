7801b2a3ff9daf3767d9bf04924cf323bf97b908
nikhilhat <nikhil.hatwar13@gmail.com>
2022-04-11 10:37:55 +0530

    Updated

--- Angular/Day32/all-in-one/src/app/app.module.ts
+++ Angular/Day32/all-in-one/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TdfDemoComponent } from './tdf-demo/tdf-demo.component';
+//import { TdfDemoComponent } from './tdf-demo/tdf-demo.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TdfDemoComponent,
+   // TdfDemoComponent,
     RegistrationFormComponent
   ],
   imports: [
